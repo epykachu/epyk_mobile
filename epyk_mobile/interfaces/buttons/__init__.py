aedccf99804938c06da0d16dcb955174bdcb4461
Tinelsi972 <epykachu@gmail.com>
2020-08-27 23:35:03 +0100

    add ImageView

--- epyk_mobile/interfaces/buttons/__init__.py
+++ epyk_mobile/interfaces/buttons/__init__.py
@@ -1,7 +1,11 @@
 from dataclasses import dataclass
-from epyk_mobile.interfaces.android import TextViewObject
+from epyk_mobile.interfaces.android import TextViewObject, ImageView
 
 
 @dataclass
 class Button(TextViewObject):
+    pass
+
+@dataclass
+class ImageButton(ImageView):
     pass
